d1bb0e9149f1afa3c11d515fd5cf9b59443b0141
Tushar Rajput <tusharrajput604@gmail.com>
2020-09-15 16:03:27 +0530

    Creating first django application

--- intro_project/intro_project/urls.py
+++ intro_project/intro_project/urls.py
@@ -1,4 +1,4 @@
-"""my_first URL Configuration
+"""intro_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
